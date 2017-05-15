4b3666baf8aa80f0ca1981b4d864b7877cf17a89
simon <sim.schmid@gmx.net>
2017-05-15 23:37:59 +0200

    version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='hddl',
     description='Downloader and converter for histdata.com',
-    version="0.1",
+    version="0.2",
     scripts=['hddl.py'],
     packages=['hddl']
         )
