6c95ca5fc0e0de8ce6ceafea2a57311089bcd047
simon <simon@buro>
2017-06-16 13:27:03 +0200

    added update capabilities

--- scripts/hddl.py
+++ scripts/hddl.py
@@ -157,7 +157,7 @@ def download(pair,fro, to,dest,freq='5s',update=True):
     print "downloading range:",daterange
     f=open(dest,'a' if file_exists else 'w')
     for date in daterange:
-        year=date.year
+        year=date.year 
         month=date.month
         df=Downloader(pair, year, month,verbose=1,freq=freq).download()
         #data=df if data is None else data.append(df)
